b8b5be3b1e2a8714d67d5691349b18ca506c3671
Julia Valentini <jvalentini555@gmail.com>
2026-08-10 22:35:44 -0400

    Add award

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -11,5 +11,6 @@ Iran's Arrow is a 78 minute film about a rather ordinary British car introduced 
     <p>
       Although dwindling in numbers, the Paykan can still be seen in the streets of Iran today. This film explores the impact the Paykan had on Iranian society, and the impact the Iranian society had on this car.
     </p>
+    <img src="../assets/award.png">
   </main>
 </template>
