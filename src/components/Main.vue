a52f835dacf5bf9c57a0c63f36395be3602788b9
jvalentini555 <59949033+jvalentini555@users.noreply.github.com>
2026-08-11 22:10:25 -0400

    Complete short synopsis

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,36 @@
 <script setup lang="ts">
+import { Icon } from '@iconify/vue'
+const accordionItems = [
+  {
+    value: 'item-1',
+    title: 'Synopsis',
+    content: 'From the spring of 1967, when it first rolled off the assembly line in the outskirts of Tehran, Paykan was continuously produced for 38 years and played a fundamental role in Iranian society.\n\nThe film features people who share a common interest for the country\'s emblematic car, from pure passion to a complex love-hate connection.\n\nIran\'s Arrow uses rare archival footage and imagery to document the history of Paykan and how it has been perceived throughout the years and examines its links with Iran\'s major historical and sociological landmarks such as the White Revolution, the industrialization, the rise of the middle class, the Islamic Revolution, the Iran-Iraq war, and the post-war era.\n\nThe film also documents the decline of Paykan, and how it is still very much alive in the culture and memory of the country.',
+  },
+  {
+    value: 'item-2',
+    title: 'Featured Interviews',
+    content: 'Yes. It\'s unstyled by default, giving you freedom over the look and feel.',
+  },
+  {
+    value: 'item-3',
+    title: 'Directors',
+    content: 'Yes! You can use the transition prop to configure the animation.',
+  },
+]
 </script>
 
 <template>
   <img src="../assets/poster.png">
   <main>
-    <h1>Short Synopsis</h1>
     <p>
 Iran's Arrow is a 78 minute film about a rather ordinary British car introduced to Iran in 1967, which during its lengthy production period of 38 years became the national car in its new homeland.<br>
     </p>
     <p>
       Although dwindling in numbers, the Paykan can still be seen in the streets of Iran today. This film explores the impact the Paykan had on Iranian society, and the impact the Iranian society had on this car.
     </p>
+    <p>Produced in Tehran 2014 - 2017</p>
+    <p><i>Written, Directed and Produced by:</i></p>
+    <p><b>Shahin Armin and Sohrab Daryabandari</b></p>
     <img src="../assets/award.png">
   </main>
 </template>
