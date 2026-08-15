b3e559d714a94ec3e723fe2352e4b4634e688d47
Julia Valentini <jvalentini555@gmail.com>
2026-08-14 21:52:47 -0400

    Add featured experts

---
+++ src/components/Accordion.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+import { AccordionContent, AccordionHeader, AccordionItem, AccordionTrigger } from 'reka-ui'
+import '../style.css'
+const props = defineProps({'title': String, 'value': String})
+</script>
+
+<template>
+  <AccordionItem 
+    class="AccordionItem"
+    value={{value}}>
+    <AccordionHeader class="AccordionHeader">
+      <AccordionTrigger class="AccordionTrigger">
+        <span>{{title}}</span>
+        <Icon
+          icon="radix-icons:chevron-down"
+          class="AccordionChevron"
+        />
+      </AccordionTrigger>
+    </AccordionHeader>
+    <AccordionContent class="AccordionContent">
+      <slot></slot>
+    </AccordionContent>
+  </AccordionItem>
+</template>
+
+<style>
+.AccordionItem {
+  background-color: #00000000;
+  border-color:#00000000;
+}
+.AccordionTrigger {
+  background-color: #00000000;
+  border-color:#00000000;
+  box-sizing:border-box;
+  width: 100%;
+  display: flex;
+  font-size: 28px;
+  text-align: left;
+  align-items: center;
+  color: #16171d;
+}
+.AccordionHeader {
+  border-bottom: 2px solid gainsboro;
+}
+.AccordionContent {
+  margin: 0;
+  font-size: 16px;
+  text-align: left;
+  padding-left: 15px;
+  padding-right: 15px;
+}
+.AccordionChevron {
+  transition: transform 300ms;
+}
+.AccordionTrigger[data-state="open"] > .AccordionChevron {
+  transform: rotate(180deg);
+}
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,62 +1,45 @@
 <script setup lang="ts">
-import { Icon } from '@iconify/vue'
-import { AccordionContent, AccordionHeader, AccordionItem, AccordionRoot, AccordionTrigger } from 'reka-ui'
+import Accordion from './Accordion.vue'
+import { AccordionRoot } from 'reka-ui'
 
-const accordionItems = [
-  {
-    value: 'item-1',
-    title: 'Synopsis',
-    content: [
-      'From the spring of 1967, when it first rolled off the assembly line in the outskirts of Tehran, Paykan was continuously produced for 38 years and played a fundamental role in Iranian society.',
-      'The film features people who share a common interest for the country\'s emblematic car, from pure passion to a complex love-hate connection.',
-      'Iran\'s Arrow uses rare archival footage and imagery to document the history of Paykan and how it has been perceived throughout the years and examines its links with Iran\'s major historical and sociological landmarks such as the White Revolution, the industrialization, the rise of the middle class, the Islamic Revolution, the Iran-Iraq war, and the post-war era.','The film also documents the decline of Paykan, and how it is still very much alive in the culture and memory of the country.'],
-  }
-]
 </script>
 
 <template>
   <img src="../assets/poster.png">
   <main>
     <p>
-Iran's Arrow is a 78 minute film about a rather ordinary British car introduced to Iran in 1967, which during its lengthy production period of 38 years became the national car in its new homeland.<br>
+      Iran's Arrow is a 78 minute film about a rather ordinary British car introduced to Iran in 1967, 
+      which during its lengthy production period of 38 years became the national car in its new homeland.<br>
     </p>
     <p>
-      Although dwindling in numbers, the Paykan can still be seen in the streets of Iran today. This film explores the impact the Paykan had on Iranian society, and the impact the Iranian society had on this car.
+      Although dwindling in numbers, the Paykan can still be seen in the streets of Iran today. 
+      This film explores the impact the Paykan had on Iranian society, and the impact the Iranian society had on this car.
     </p>
     <p>Produced in Tehran 2014 - 2017</p>
     <p><i>Written, Directed and Produced by:</i></p>
-    <p><b>Shahin Armin and Sohrab Daryabandari</b></p>
+    <p><b>Shahin Armin and Sohrab Daryabandari</b></p><br>
     <img src="../assets/award.png">
   </main>
   <AccordionRoot
     type="single"
     :collapsible="true"
   >
-    <template
-      v-for="item in accordionItems"
-      :key="item.value"
-    >
-      <AccordionItem
-        :value="item.value"
-      >
-        <AccordionHeader>
-          <AccordionTrigger>
-            <span>{{ item.title }}</span>
-            <Icon
-              icon="radix-icons:chevron-down"
-              class="AccordionChevron"
-            />
-          </AccordionTrigger>
-        </AccordionHeader>
-        <AccordionContent class="text-mauve11 bg-mauve2 data-[state=open]:animate-slideDown data-[state=closed]:animate-slideUp overflow-hidden text-sm">
-          <p 
-            v-for="(part, index) in item.content"
-            :key="index"
-          >
-            {{ part }}<br>
-          </p>
-        </AccordionContent>
-      </AccordionItem>
-    </template>
+    <Accordion title="Synopsis" value="item-1">
+      From the spring of 1967, when it first rolled off the assembly line in the outskirts of Tehran, 
+      Paykan was continuously produced for 38 years and played a fundamental role in Iranian society.<br><br>
+      The film features people who share a common interest for the country's emblematic car, 
+      from pure passion to a complex love-hate connection.<br><br>
+      Iran's Arrow uses rare archival footage and imagery to document the history of Paykan and 
+      how it has been perceived throughout the years and examines its links with Iran's major historical 
+      and sociological landmarks such as the White Revolution, the industrialization, the rise of the middle class, 
+      the Islamic Revolution, the Iran-Iraq war, and the post-war era.<br><br>
+      The film also documents the decline of Paykan, and how it is still very much alive in the culture and memory of the country.<br><br>
+    </Accordion>
+    <Accordion title="Featured Experts" value="item-2">
+      <div>
+        <img src="../assets/talking-head-amir-tehrani.png">
+      </div>
+    </Accordion>
   </AccordionRoot>
 </template>
+
